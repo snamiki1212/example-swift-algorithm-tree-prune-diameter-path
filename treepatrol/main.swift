e5a7802171f1e6adfc311da1799dae6184639f7f
snamiki1212 <snamiki1212@gmail.com>
2021-08-17 17:56:20 -0700

    Add skelton

---
+++ treepatrol/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  treepatrol
+//
+//  Created by shunnamiki on 2021/08/17.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ treepatrol/solution.swift
@@ -0,0 +1,30 @@
+//
+//  solution.swift
+//  treepatrol
+//
+//  Created by shunnamiki on 2021/08/17.
+//
+
+import Foundation
+
+// copy-paste all your classes/structs used in the program.
+class Queue {}
+struct Edge {}
+class Graph {}
+// ...
+
+func solution() {
+    // your main logic (input processing, algorithm, etc.)
+
+    // remove all your debugging print statements
+
+    // print the result (make sure you follow the output specification)
+}
+
+// you can include as many helper functions as you want.
+func helper1() {}
+
+func heloper2() {}
+
+// make sure you run the function (before you submit)
+//solution()
